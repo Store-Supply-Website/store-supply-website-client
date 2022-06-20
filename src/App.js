e25e2b0ce13fbcd056d6600b771bfb587408e2b2
Marisa Shang <yutong.shang.vanderbilt@gmail.com>
2022-06-20 11:01:00 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,3 +22,59 @@ function App () {
 }
 
 export default App
+
+
+//New updates below
+//Import models in App.js for Add user form, Yutong S.
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var flash = require('express-flash');
+var session = require('express-session');
+//可能需要更新path
+var usersRouter = require('./routes/users');
+ 
+ 
+var app = express();
+ 
+// view engine setup
+//可能不需要
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+ 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+app.use(session({ 
+    secret: '123456catr',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}))
+ 
+app.use(flash());
+app.use('/', usersRouter);
+ 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+ 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+ 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+ 
+module.exports = app;
