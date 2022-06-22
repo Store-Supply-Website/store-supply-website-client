c53b021272611975b77c4ab9dc19c25a4dbca8b2
Marisa Shang <yutong.shang.vanderbilt@gmail.com>
2022-06-21 20:20:30 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg'
 import './App.css'
 
+
+
 import Home from './pages/Home'
 //import Home from './pages/test'
 import Profile from './pages/Profile'
@@ -14,7 +16,8 @@ function App () {
       {/* <Link to="/" className="link">HomePage</Link>
       <Link to="/profile" className="link">Profile</Link> */}
       <Routes>
-        <Route path='/' element={<Login />}></Route>
+        {/* <Route path='/' element={<Login />}></Route> */}
+        <Route path='/' element={<Up_Info />}></Route>
         <Route path='/register' element={<Register />}></Route>
         <Route path='/home' element={<Home />}></Route>
         <Route path='/profile' element={<Profile />}></Route>
@@ -25,59 +28,3 @@ function App () {
 }
 
 export default App
-
-
-//New updates below
-//Import models in App.js for Add user form, Yutong S.
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var flash = require('express-flash');
-var session = require('express-session');
-//可能需要更新path
-var usersRouter = require('./routes/users');
- 
- 
-var app = express();
- 
-// view engine setup
-//可能不需要
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
- 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
- 
-app.use(session({ 
-    secret: '123456catr',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 60000 }
-}))
- 
-app.use(flash());
-app.use('/', usersRouter);
- 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
- 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
- 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
- 
-module.exports = app;
