3fcfeccb246ac4a5d6ff34dd70983a9f88f80849
Marisa Shang <yutong.shang.vanderbilt@gmail.com>
2022-06-22 19:58:39 -0700

    Create phone component

---
+++ src/components/Phone.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MuiPhoneNumber from 'material-ui-phone-number'
+
+
+
+
+function Phone () {
+
+  return (
+    <MuiPhoneNumber defaultCountry='us' />
+  )
+}
+
+export default Phone
