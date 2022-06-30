e6aea5109d8e2bbd6ed3f69a9e9aa937657b476c
Marisa Shang <yutong.shang.vanderbilt@gmail.com>
2022-06-29 22:29:51 -0700

    Fix bug in displaying user info

--- src/pages/User.js
+++ src/pages/User.js
@@ -4,9 +4,28 @@ import TitlebarImageList from '../components/CommodityList'
 import { Tabs, Tab, Grid, Paper, Button } from '@mui/material'
 import EditIcon from '@mui/icons-material/Edit'
 import { Link } from "react-router-dom"
-function User () {
-  console.log()
+import Typography from '@mui/material/Typography'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Collapse from '@mui/material/Collapse'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import { red } from '@mui/material/colors'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ShareIcon from '@mui/icons-material/Share'
+import { Register_URL, UPDATE_URL, LOGIN_URL, SEARCH_URL } from '../utils/api'
 
+function User () {
+  console.log('hi')
+  console.log(sessionStorage)
+  const curUser = sessionStorage.getItem('user')
+  console.log(curUser)
+  const curUserObj = JSON.parse(curUser)
+  const curId = curUserObj._id
+  console.log(curId)
   const styles = {
     paper: {
       marginTop: 20,
@@ -15,48 +34,94 @@ function User () {
     }
   }
 
-  // const clickHandler = () => {
-  //   console.log('clicked')
-  // }
+  const SendRegisterRequest = async (LoginData) => {
+    const curUser = sessionStorage.getItem('user')
+    const curUserObj = JSON.parse(curUser)
+    const curId = curUserObj._id
+    console.log(curId)
+    try {
+      //build post request params
+      const params = {
+        method: 'GET',
+        body: JSON.stringify({ id: curId }),
+        // headers: { 'Content-Type': 'application/json' }
+      }
+      console.log("hiiiii")
+      console.log(SEARCH_URL + curId)
+      const createResponse = await fetch(SEARCH_URL + curId, params)
+      const newData = await createResponse.json()
+      //process register request response
+      const code = newData['status']
+      if (code === 200) {
+        console.log("test1")
+        alert("fetch successfully!")
+        // navigate('/user')
 
-  console.log(styles.paper)
+      } else {
+        alert(newData['message'])
+      }
+      console.log(newData)
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+  SendRegisterRequest()
+
   return (
-    <div >
+    <div>
       <NaviBar></NaviBar>
-      <Grid container spacing={2} alignItems="stretch" justify="center" direction="column">
-        <Grid item xs={12}>
-          <Paper style={styles.paper}>
-            User Info
-            User Info
-          </Paper>
-        </Grid>
-        <Grid item xs={12}>
-          <Paper style={styles.paper}>
-            Supplies
-            Supplies
-          </Paper>
-        </Grid>
-      </Grid>
-      <Grid>
-        <Button
-          variant="contained"
-          startIcon={<EditIcon />}
-          color="primary" type="submit"
-          // onClick={clickHandler}
-          component={Link}
-          to="/update"
+      <Paper style={styles.paper}
+        sx={{
+          p: 2,
+          margin: 'auto',
+          maxWidth: 500,
+          flexGrow: 1,
+          backgroundColor: '#fff'
+        }}>
+        <Grid container spacing={20} alignItems="stretch" justify="center" direction="column"
         >
-          Edit profile
-        </Button>
-      </Grid>
-      {/* <Tabs
-        value={0}
-        variant="fullWidth"
-        indicatorColor="primary"
-        aria-label="basic tabs example">
-        <Tab label="User Info" />
-        <Tab label="Items" />
-      </Tabs> */}
+          <Grid item xs={20}>
+
+            <Typography variant="body2" gutterBottom>
+              Username
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {curUserObj.username}
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              Email
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {curUserObj.email}
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              Phone
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {curUserObj.phone}
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              Address
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {curUserObj.address}
+            </Typography>
+          </Grid>
+
+        </Grid>
+        <Grid>
+          <Button
+            variant="contained" fullWidth='true'
+            startIcon={<EditIcon />}
+            color="primary" type="submit"
+            component={Link}
+            to="/update"
+          >
+            Edit profile
+          </Button>
+        </Grid>
+      </Paper>
     </div>
 
   )
