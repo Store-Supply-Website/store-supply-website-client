2b3dee2d94adc35133dd7caf90d7f2b6f8e34700
wangyan66 <819156618@qq.com>
2022-06-30 23:46:16 -0700

    Fix: fix sign up bugs and searchbar ui

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -170,12 +170,12 @@ export default function SignIn () {
               >
                 Sign In
               </Button>
-              <Grid container>
-                <Grid item xs>
+              <Grid container justifyContent="flex-end">
+                {/* <Grid item xs>
                   <Link href="#" variant="body2">
                     Forgot password?
                   </Link>
-                </Grid>
+                </Grid> */}
                 <Grid item>
                   <Link href="register" variant="body2">
                     {"Don't have an account? Sign Up"}
--- src/pages/Commodity.js
+++ src/pages/Commodity.js
@@ -1,6 +1,7 @@
 import '../App.css'
 import TitlebarImageList from '../components/CommodityList'
 import { NaviBar } from '../components/NaviBar'
+import CustomizedInputBase from '../components/SerachBar'
 function Commodity () {
   return (
     <div>
@@ -9,12 +10,17 @@ function Commodity () {
 
       </header>
       <article>
+        <div className='flexbox-centering'>
+          <CustomizedInputBase sx={{ mt: 100 }}></CustomizedInputBase>
+        </div>
+
         <div className='flexbox-centering' >
+
           <TitlebarImageList isCom={true} isMy={false} />
         </div>
 
-      </article>
-    </div>
+      </article >
+    </div >
   )
 }
 export default Commodity
